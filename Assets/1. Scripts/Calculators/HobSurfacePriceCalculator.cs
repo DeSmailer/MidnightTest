7dc084394e1575f9ea194ee6994e5b946f47b1c2
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-04-18 17:02:30 +0300

    balance table created

--- Assets/1. Scripts/Calculators/HobSurfacePriceCalculator.cs
+++ Assets/1. Scripts/Calculators/HobSurfacePriceCalculator.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public static class HobSurfacePriceCalculator
 {
+    //https://docs.google.com/spreadsheets/d/1SDo__HAHj6sR4IdLvgtswTXHGXDqqHFnFjLwR45mGTs/edit?usp=sharing
+
     public static double GetPurchasePrice(int serialNumberUponPurchase)
     {
         double res = Math.Pow(7 * serialNumberUponPurchase, serialNumberUponPurchase);
